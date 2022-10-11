b8abdf608ec7ec4733c6b1abd1bfbe1d4147f370
wyf <wyf000219@gmail.com>
2022-10-11 16:34:10 +0800

    Update 04_load/start_gpfdist.sh

--- 04_load/start_gpfdist.sh
+++ 04_load/start_gpfdist.sh
@@ -4,10 +4,10 @@ set -e
 PWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 GPFDIST_PORT=${1}
 GEN_DATA_PATH=${2}
+gphome=${3}
 
 if [ -z "$GPHOME" ]; then
-    echo "empty string"
-    source /home/gpadmin/install/gpdb/greenplum_path.sh
+    source $gphome/greenplum_path.sh
 fi
 
 gpfdist -p ${GPFDIST_PORT} -d ${GEN_DATA_PATH} > gpfdist.${GPFDIST_PORT}.log 2>&1 < gpfdist.${GPFDIST_PORT}.log &
