2f4648a784e9e7f09e31099b8ccaf590f3ed132f
Ryan Wei <rainmanwy@gmail.com>
2022-11-11 16:44:01 +0800

    bugfix

--- 03_ddl/rollout.sh
+++ 03_ddl/rollout.sh
@@ -8,14 +8,14 @@ init_log ${step}
 get_version
 
 filter="gpdb"
+schema_name=${SCHEMA_NAME}
+ext_schema_name= "ext_${SCHEMA_NAME}"
 
 if [ "${DROP_EXISTING_TABLES}" == "true" ]; then
   #Create tables
   for i in $(ls ${PWD}/*.${filter}.*.sql); do
     file_name=$(echo ${i} | awk -F '/' '{print $NF}')
     id=$(echo ${file_name} | awk -F '.' '{print $1}')
-    schema_name=${SCHEMA_NAME}
-    ext_schema_name= "ext_${SCHEMA_NAME}"
     table_name=$(echo ${file_name} | awk -F '.' '{print $3}')
     start_log
 
