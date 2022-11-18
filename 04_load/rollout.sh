8b038564271c3b4c252724d685e05f1e721045f3
Ryan Wei <rainmanwy@gmail.com>
2022-11-18 18:08:35 +0800

    Add paralle for large cluster

--- 04_load/rollout.sh
+++ 04_load/rollout.sh
@@ -45,9 +45,9 @@ function start_gpfdist()
     GEN_DATA_PATH="${GEN_DATA_PATH}/hbenchmark"
     PORT=$((GPFDIST_PORT + CHILD))
     echo "executing on ${EXT_HOST} ./start_gpfdist.sh $PORT ${GEN_DATA_PATH}"
-    ssh -n -f ${EXT_HOST} "bash -c 'cd ~${ADMIN_USER}; source ./.bash_profile; ./start_gpfdist.sh $PORT ${GEN_DATA_PATH}'"
-    sleep 1
+    ssh -n -f ${EXT_HOST} "bash -c 'cd ~${ADMIN_USER}; source ./.bash_profile; ./start_gpfdist.sh $PORT ${GEN_DATA_PATH}'" &
   done
+  wait
 }
 
 copy_script
